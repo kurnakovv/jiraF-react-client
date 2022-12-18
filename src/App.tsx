265ed49a6b34e8c16fa2c44ca89c29d95c949be7
KurnakovMaksim <maksk210@gmail.com>
2022-12-18 16:45:09 +0900

    #1 Test changes for check deploy

--- src/App.tsx
+++ src/App.tsx
@@ -74,6 +74,7 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Deployed from GitHub Pages</h1>
       {isAddedGoal 
         ? <><h1>Added new goal!</h1><button onClick={() => setIsAddedGoal(false)}>X</button></>
         : <></>
