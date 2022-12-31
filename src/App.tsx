cbb2db6903b38ff081ecdd95eddfe3e21ca3874d
KurnakovMaksim <maksk210@gmail.com>
2022-12-31 16:54:33 +0900

    Delete test header

--- src/App.tsx
+++ src/App.tsx
@@ -74,7 +74,6 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Test 2</h1>
       {isAddedGoal 
         ? <><h1>Added new goal!</h1><button onClick={() => setIsAddedGoal(false)}>X</button></>
         : <></>
